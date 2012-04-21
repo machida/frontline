322a8e587e93aed48f909fa05b10206bc37e7f8a
machida <machida@fjord.jp>
2012-04-22 03:46:33 +0900

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontline::Application.config.secret_token = 'f278c579b7ce15e44fb352c945dc8158eaad843c116a2f9cfa3d60f57523197d7caa1f07d50ae42a1aef9c2b78dc640cce0d5062c611203c4797da1201591afa'
